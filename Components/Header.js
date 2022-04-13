fbcb48b33cd7e483e1ecbb603905db6838609580
Jesus Politron <polipop.work@gmail.com>
2022-04-13 14:00:15 -0500

    added images folder, added filter section component and the filter basic styles as a module

--- Components/Header.js
+++ Components/Header.js
@@ -5,19 +5,19 @@ export default function Header() {
     return (
         <navbar className={styles.navbar}>
             <button>
-                <img src="https://www.flaticon.es/svg/vstatic/svg/3916/3916837.svg?token=exp=1649623327~hmac=1770dab025ee17baeb66689b6105d98b" />
+                <img src="https://www.flaticon.es/svg/vstatic/svg/3916/3916810.svg?token=exp=1649870193~hmac=12bbe46ebaae643491f8c67be9f1e67d" />
             </button>
 
             <button>
-                <img src="https://www.flaticon.es/svg/vstatic/svg/3917/3917559.svg?token=exp=1649624895~hmac=fe8a02df9b46e62a0a3e88df85ec5ce2" />
+                <img src="https://www.flaticon.es/svg/vstatic/svg/3917/3917688.svg?token=exp=1649870133~hmac=e2c7cb09179fc7e45513ec288186fd56" />
             </button>
 
             <button>
-                <img src="https://www.flaticon.es/svg/vstatic/svg/3914/3914366.svg?token=exp=1649625504~hmac=fd20b830f26635f275f2f95b728a6e7f" />
+                <img src="https://www.flaticon.es/svg/vstatic/svg/3914/3914366.svg?token=exp=1649869010~hmac=5b86e23d9cb39c5eddcfcd9aa9994b3c" />
             </button>
 
             <button>
-                <img src="https://www.flaticon.es/svg/vstatic/svg/3917/3917132.svg?token=exp=1649625533~hmac=75b9a9619468291b36b1857ba61862c1" />
+                <img src="https://www.flaticon.es/svg/vstatic/svg/3917/3917132.svg?token=exp=1649870133~hmac=02ce4cedea01b77bac55aba4804c2bb3" />
             </button>
         </navbar>
     );
--- pages/loginView.js
+++ pages/loginView.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import Header from "../Components/Header";
 
 import styles from "../styles/Login.module.css";
 
