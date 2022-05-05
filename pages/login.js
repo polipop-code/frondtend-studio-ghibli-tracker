c02687c1515c60738144dc849b16ecf19f10a488
Jesus Politron <polipop.work@gmail.com>
2022-05-05 15:09:05 -0500

    added wrapper divs for align items, fixed some design views, functionality still pendant

--- pages/login.js
+++ pages/login.js
@@ -11,61 +11,63 @@ const userDB = [
 
 export default function LoginView() {
   return (
-    <div className={styles.Login_container}>
-      <h1>Studio Ghibli Tracker</h1>
+    <div className={styles.Login_wrapper}>
+      <div className={styles.Login_container}>
+        <h1>Studio Ghibli Tracker</h1>
 
-      <form className={styles.Login_form} action="/" method="get">
-        <input
-          type="email"
-          placeholder="Your email"
-          id="user_email"
-          name="user_email"
-          required
-          minLength="4"
-        ></input>
+        <form className={styles.Login_form} action="/" method="get">
+          <input
+            type="email"
+            placeholder="Your email"
+            id="user_email"
+            name="user_email"
+            required
+            minLength="4"
+          ></input>
 
-        <input
-          type="password"
-          placeholder="Your password"
-          id="user_password"
-          name="user_password"
-          required
-          minLength="8"
-        ></input>
+          <input
+            type="password"
+            placeholder="Your password"
+            id="user_password"
+            name="user_password"
+            required
+            minLength="8"
+          ></input>
 
-        <button
-          type="submit"
-          onClick={function (event) {
-            var userVal = document.getElementById("user_email").value;
-            var userPasswordVal = document.getElementById("user_password").value;
-            if ((userVal == userDB[0].email) & (userPasswordVal == userDB[0].password)) {
-              alert("Usuario Valido");
-            } else {
-              event.preventDefault();
-              alert("Algo salio mal");
-            }
-          }}
-        >
-          Login
-        </button>
-        <a href="#">Forgot your password?</a>
-      </form>
+          <button
+            type="submit"
+            onClick={function (event) {
+              var userVal = document.getElementById("user_email").value;
+              var userPasswordVal = document.getElementById("user_password").value;
+              if ((userVal == userDB[0].email) & (userPasswordVal == userDB[0].password)) {
+                alert("Usuario Valido");
+              } else {
+                event.preventDefault();
+                alert("Algo salio mal");
+              }
+            }}
+          >
+            Login
+          </button>
+          <a href="#">Forgot your password?</a>
+        </form>
 
-      <section className={styles.Login_signin}>
-        <p>Don't have an account?</p>
-        <Link href="/onboarding">
-          <button type="button">Signin</button>
-        </Link>
-      </section>
+        <section className={styles.Login_signin}>
+          <p>Don't have an account?</p>
+          <Link href="/onboarding">
+            <a type="button">Signin</a>
+          </Link>
+        </section>
 
-      <section className={styles.Login_socialMedia}>
-        <button className={styles.Login_socialMedia_Facebook} disabled>
-          Connect with Facebook
-        </button>
-        <button className={styles.Login_socialMedia_Twitter} disabled>
-          Connect with Twitter
-        </button>
-      </section>
+        <section className={styles.Login_socialMedia}>
+          <button className={styles.Login_socialMedia_Facebook} disabled>
+            Connect with Facebook
+          </button>
+          <button className={styles.Login_socialMedia_Twitter} disabled>
+            Connect with Twitter
+          </button>
+        </section>
+      </div>
     </div>
   );
 }
--- pages/onboarding.js
+++ pages/onboarding.js
@@ -4,37 +4,39 @@ import styles from "../styles/Onboarding.module.css";
 
 export default function OnboardingView() {
   return (
-    <div className={styles.Onboarding_container}>
-      <h1>Studio Ghibli Tracker</h1>
+    <div className={styles.Onboarding_wrapper}>
+      <div className={styles.Onboarding_container}>
+        <h1>Studio Ghibli Tracker</h1>
 
-      <form className={styles.signin_form} action="/login" method="post">
-        <input
-          type="email"
-          placeholder="Enter a valid email"
-          id="user_email"
-          name="user_email"
-          required
-          minLength="4"
-        ></input>
+        <form className={styles.signin_form} action="/login" method="post">
+          <input
+            type="email"
+            placeholder="Enter a valid email"
+            id="user_email"
+            name="user_email"
+            required
+            minLength="4"
+          ></input>
 
-        <input
-          type="password"
-          placeholder="Enter a password"
-          id="user_password"
-          name="user_password"
-          required
-          minLength="8"
-        ></input>
+          <input
+            type="password"
+            placeholder="Enter a password"
+            id="user_password"
+            name="user_password"
+            required
+            minLength="8"
+          ></input>
 
-        <button type="submit">Register</button>
-      </form>
+          <button type="submit">Register</button>
+        </form>
 
-      <section className={styles.Onboarding_login}>
-        <p>Do you have an account?</p>
-        <Link href="/login">
-          <button type="button">Login here</button>
-        </Link>
-      </section>
+        <section className={styles.Onboarding_login}>
+          <p>Do you have an account?</p>
+          <Link href="/login">
+            <a type="button">Login</a>
+          </Link>
+        </section>
+      </div>
     </div>
   );
 }
